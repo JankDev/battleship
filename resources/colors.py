d779d205f98b0b4e3a5a0b0266f51ef242d259f7
JankDev <robert.s.kraut@gmail.com>
2018-12-31 17:33:30 +0100

    refactored

---
+++ resources/colors.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+DARK_GREEN = (0, 100, 0)
+GOLD = (255, 215, 0)
+VIOLET = (102, 0, 102)
+AQUA = (102, 255, 255)
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
---
+++ resources/fonts.py
@@ -0,0 +1,8 @@
+import pygame
+
+table_font = pygame.font.Font(pygame.font.get_default_font(), 20)
+font = pygame.font.Font(pygame.font.get_default_font(), 50)
+small_font = pygame.font.Font(pygame.font.get_default_font(), 30)
+game_over_font = pygame.font.Font(pygame.font.get_default_font(), 300)
+head_font = pygame.font.Font(pygame.font.get_default_font(), 70)
+INPUT_FONT = pygame.font.Font(None, 50)
